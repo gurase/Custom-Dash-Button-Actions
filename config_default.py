601695037aeb06a5adb6df76191c887e7061a47b
gurase <gracemariemann@gmail.com>
2016-07-14 17:04:45 -0400

    Added config_default.py

---
+++ config_default.py
@@ -0,0 +1,15 @@
+# Wunderlist credentials
+CLIENT_ID =
+ACCESS_TOKEN =
+
+# Dash buttons
+buttons = {
+    "button_mac1": {
+        "list": "list1",
+        "message": "message1"
+    },
+    "button_mac2": {
+        "list": "list2",
+        "message": "message2"
+    }
+}
