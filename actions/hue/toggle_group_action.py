c92f68f23e90c484c29c31ad7deefcc9b12af85e
gurase <gracemariemann@gmail.com>
2016-07-16 10:15:51 -0400

    Renamed config variables

--- actions/hue/toggle_group_action.py
+++ actions/hue/toggle_group_action.py
@@ -6,5 +6,5 @@ class ToggleGroupAction(Action):
     def __init__(self, group_name):
         self.group_name = group_name
     def execute(self):
-        client = HueClient(config.IP, config.USER)
+        client = HueClient(config.HUE_IP, config.HUE_USER)
         client.toggle_group(self.group_name)
